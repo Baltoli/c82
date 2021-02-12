abed076d6303f8e570eed283a24398f99343fb92
Bruce Collie <brucecollie82@gmail.com>
2021-02-12 14:57:32 +0000

    Starting to write

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::fmt;
+
+const RAM_SIZE: usize = 4096;
+const REGISTERS: usize = 16;
+const STACK_SIZE: usize = 16;
+
+struct Device {
+    ram: [u8; RAM_SIZE],
+    stack: [u16; STACK_SIZE],
+    registers: [u8; REGISTERS],
+    reg_i: u8,
+    delay: u8,
+    sound: u8,
+    pc: u16,
+    sp: u8,
+}
+
+impl Device {
+    fn new() -> Self {
+        Device {
+            ram: [0; RAM_SIZE],
+            stack: [0; STACK_SIZE],
+            registers: [0; REGISTERS],
+            reg_i: 0,
+            delay: 0,
+            sound: 0,
+            pc: 0,
+            sp: 0,
+        }
+    }
+}
+
+impl fmt::Display for Device {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "(device)")
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let d = Device::new();
+
+    println!("{}", d);
 }
