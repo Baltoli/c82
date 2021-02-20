2de52f8df3a31320c7f70658f299c0f497f49384
Bruce Collie <brucecollie82@gmail.com>
2021-02-20 15:49:24 +0000

    Display

--- src/main.rs
+++ src/main.rs
@@ -10,63 +10,85 @@ const STACK_SIZE: usize = 16;
 const DISPLAY_WIDTH: usize = 64;
 const DISPLAY_HEIGHT: usize = 32;
 const PIXEL_SIZE: usize = 8;
-const DISPLAY_PIXELS: usize = DISPLAY_WIDTH * PIXEL_SIZE * DISPLAY_HEIGHT * PIXEL_SIZE;
 
-struct Device {
-    ram: [u8; RAM_SIZE],
-    stack: [u16; STACK_SIZE],
-    registers: [u8; REGISTERS],
-    display: [u32; DISPLAY_PIXELS],
-    reg_i: u8,
-    delay: u8,
-    sound: u8,
-    pc: u16,
-    sp: u8,
-    window: minifb::Window,
+struct Display {
+    width: usize,
+    height: usize,
+    scale: usize,
+    buffer: Vec<u32>,
+    window: Window,
 }
 
-impl Device {
-    fn new() -> Self {
-        Device {
-            ram: [0; RAM_SIZE],
-            stack: [0; STACK_SIZE],
-            registers: [0; REGISTERS],
-            display: [0; DISPLAY_PIXELS],
-            reg_i: 0,
-            delay: 0,
-            sound: 0,
-            pc: 0,
-            sp: 0,
-            window: minifb::Window::new(
+impl Display {
+    fn new(width: usize, height: usize, scale: usize) -> Self {
+        Display {
+            width,
+            height,
+            scale,
+            buffer: vec![0; width * height * scale * scale],
+            window: Window::new(
                 "Chip8",
-                DISPLAY_WIDTH * PIXEL_SIZE,
-                DISPLAY_HEIGHT * PIXEL_SIZE,
+                width * scale,
+                height * scale,
                 WindowOptions::default(),
             )
             .unwrap(),
         }
     }
 
-    fn step(&mut self) {
+    fn refresh(&mut self) {
         self.window
             .update_with_buffer(
-                &self.display,
-                DISPLAY_WIDTH * PIXEL_SIZE,
-                DISPLAY_HEIGHT * PIXEL_SIZE,
+                &self.buffer,
+                self.width * self.scale,
+                self.height * self.scale,
             )
             .unwrap();
     }
+
+    fn is_open(&self) -> bool {
+        self.window.is_open()
+    }
 }
 
-const WIDTH: usize = 640;
-const HEIGHT: usize = 360;
+struct Device {
+    display: Display,
+    ram: [u8; RAM_SIZE],
+    stack: [u16; STACK_SIZE],
+    registers: [u8; REGISTERS],
+    reg_i: u8,
+    delay: u8,
+    sound: u8,
+    pc: u16,
+    sp: u8,
+}
+
+impl Device {
+    fn new() -> Self {
+        Device {
+            display: Display::new(DISPLAY_WIDTH, DISPLAY_HEIGHT, PIXEL_SIZE),
+            ram: [0; RAM_SIZE],
+            stack: [0; STACK_SIZE],
+            registers: [0; REGISTERS],
+            reg_i: 0,
+            delay: 0,
+            sound: 0,
+            pc: 0,
+            sp: 0,
+        }
+    }
+
+    fn step(&mut self) -> bool {
+        self.display.refresh();
+
+        self.display.is_open()
+    }
+}
 
 fn main() {
     let mut device = Device::new();
 
-    loop {
-        device.step();
-    }
+    while device.step() {}
 
     // let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
