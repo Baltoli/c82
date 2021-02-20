a71807c19dedea2df5e6a96dc6aae164dd4b01e4
Bruce Collie <brucecollie82@gmail.com>
2021-02-20 15:28:11 +0000

    Start to show

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,22 @@ use minifb::{Key, Window, WindowOptions};
 const RAM_SIZE: usize = 4096;
 const REGISTERS: usize = 16;
 const STACK_SIZE: usize = 16;
+const DISPLAY_WIDTH: usize = 64;
+const DISPLAY_HEIGHT: usize = 32;
+const PIXEL_SIZE: usize = 8;
+const DISPLAY_PIXELS: usize = DISPLAY_WIDTH * PIXEL_SIZE * DISPLAY_HEIGHT * PIXEL_SIZE;
 
 struct Device {
     ram: [u8; RAM_SIZE],
     stack: [u16; STACK_SIZE],
     registers: [u8; REGISTERS],
+    display: [u32; DISPLAY_PIXELS],
     reg_i: u8,
     delay: u8,
     sound: u8,
     pc: u16,
     sp: u8,
+    window: minifb::Window,
 }
 
 impl Device {
@@ -25,21 +31,30 @@ impl Device {
             ram: [0; RAM_SIZE],
             stack: [0; STACK_SIZE],
             registers: [0; REGISTERS],
+            display: [0; DISPLAY_PIXELS],
             reg_i: 0,
             delay: 0,
             sound: 0,
             pc: 0,
             sp: 0,
+            window: minifb::Window::new(
+                "Chip8",
+                DISPLAY_WIDTH * PIXEL_SIZE,
+                DISPLAY_HEIGHT * PIXEL_SIZE,
+                WindowOptions::default(),
+            )
+            .unwrap(),
         }
     }
 
-    fn timer_tick(&mut self) {
-        self.delay = self.delay.saturating_sub(1);
-        self.sound = self.sound.saturating_sub(1);
-    }
-
-    fn timers_active(&self) -> bool {
-        self.delay > 0 || self.sound > 0
+    fn step(&mut self) {
+        self.window
+            .update_with_buffer(
+                &self.display,
+                DISPLAY_WIDTH * PIXEL_SIZE,
+                DISPLAY_HEIGHT * PIXEL_SIZE,
+            )
+            .unwrap();
     }
 }
 
@@ -47,22 +62,25 @@ const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
 fn main() {
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let mut device = Device::new();
 
-    let mut window = Window::new(
-        "Test - ESC to exit",
-        WIDTH,
-        HEIGHT,
-        WindowOptions::default(),
-    )
-    .unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
+    loop {
+        device.step();
+    }
 
-    // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros(16600 * 2)));
+    // let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    // while window.is_open() && !window.is_key_down(Key::Escape) {
-    // window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
-    // }
+    // let mut window = Window::new(
+    //     "Test - ESC to exit",
+    //     WIDTH,
+    //     HEIGHT,
+    //     WindowOptions::default(),
+    // )
+
+    // // Limit to max ~60 fps update rate
+    // window.limit_update_rate(Some(std::time::Duration::from_micros(16600 * 2)));
+
+    // // while window.is_open() && !window.is_key_down(Key::Escape) {
+    // // window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    // // }
 }
